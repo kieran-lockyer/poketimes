7663a257cde7d987a2deadd5abe2d5b99e810fb1
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-06-25 21:08:59 +1000

    refactored navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { NavLink, withRouter } from 'react-router-dom'
 import { login, logout } from '../redux/actions/userAction'
 
 const Navbar = (props) => {
-    const { user } = props
-    const contextualMenu = !user.name ? (
-        <nav className="nav-wrapper red darken-3">
-            <div className="container">
-                <NavLink to='/' className="brand-logo">Poke'Times</NavLink>
-                <ul className='right'>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
-                    <li><NavLink to="/" onClick={props.login}>Sign In</NavLink></li>
-                </ul>
-            </div>
-        </nav>
-    ) : (
-        <nav className="nav-wrapper red darken-3">
-            <div className="container">
-                <NavLink to='/' className="brand-logo">Poke'Times</NavLink>
-                <ul className='right'>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
-                    <li><NavLink to="/contact">{user.name}</NavLink></li>                        
-                    <li><NavLink to="/" onClick={props.logout}>Log Out</NavLink></li>
-                </ul>
-            </div>
-        </nav>
-        )
+    const { user, login, logout } = props
+        
     return (
-        contextualMenu
+        <nav className="nav-wrapper red darken-3">
+            <div className="container">
+                <NavLink to='/' className="brand-logo">Poke'Times</NavLink>
+                <ul className='right'>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                    {user.name ? (
+                        <Fragment>
+                            <li><NavLink to="/contact">{user.name}</NavLink></li>
+                            <li><NavLink to="/" onClick={logout}>Log Out</NavLink></li>
+                        </Fragment>
+                    ) : (
+                        <li><NavLink to="/" onClick={login}>Sign In</NavLink></li>
+                    ) }
+                </ul>
+            </div>
+        </nav>
     )
 }
 
