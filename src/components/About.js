f450da600ae94c60f9c7480e715aeff0e42b7486
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-06-25 23:24:08 +1000

    removed hoc

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Rainbow from '../hoc/Rainbow'
 
 const About = () => {
     return(
@@ -10,4 +9,4 @@ const About = () => {
     )
 }
 
-export default Rainbow(About)
+export default About
